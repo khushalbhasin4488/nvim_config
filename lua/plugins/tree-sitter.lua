ffd4342efd0ca554fb1c334e137d2b49d03ba9c1
Khushal Bhasin <khushalbhasin4488@gmail.com>
2025-11-09 21:33:36 +0530

    changes

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,23 +1,26 @@
 return {
-     "nvim-treesitter/nvim-treesitter",
-     build = ":TSUpdate",
-     config = function()
-	 local configs = require("nvim-treesitter.configs")
-	 configs.setup({
-	     highlight = {
-		 enable = true,
-	     },
-	     indent= { enable = true},
-	     autotags = { enable = true },
-	     ensure_installed = {
-		 "lua",
-		 "tsx",
-		 "typescript",
-		 "go"
-	     } ,
-	     auto_install = false,
-	 })
-     end
-	
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            highlight = {
+                enable = true,
+            },
+            indent = { enable = true },
+            autotags = { enable = true },
+            ensure_installed = {
+                "lua",
+                "tsx",
+                "typescript",
+                "go",
+                "kotlin",
+                "java",
+                "python",
+            },
+            auto_install = true,
+        })
+    end
+
 
 }
